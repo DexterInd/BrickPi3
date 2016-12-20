7a4a3ba4b9f5603cbdb16afc1592cb634f889737
Matthew Richardson <matt@dexterindustries.com>
2016-12-20 18:31:42 -0500

    Fixed firmware update script directories

--- Firmware/brickpi3samd_flash_firmware.sh
+++ Firmware/brickpi3samd_flash_firmware.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Updating the BrickPi3 SAMD Firmware with '/home/pi/BrickPi3/Firmware/brickpi3_firmware.bin'."
-sudo openocd -f interface/raspberrypi2-native.cfg -c "transport select swd; set CHIPNAME at91samd21j18; source [find target/at91samdXX.cfg]; adapter_khz 250; adapter_nsrst_delay 100; adapter_nsrst_assert_width 100" -c "init; targets; reset halt; program /home/pi/BrickPi3/Firmware/brickpi3_firmware.bin verify; reset" -c "shutdown"
+echo "Updating the BrickPi3 SAMD Firmware with '/home/pi/Dexter/BrickPi3/Firmware/brickpi3_firmware.bin'."
+sudo openocd -f interface/raspberrypi2-native.cfg -c "transport select swd; set CHIPNAME at91samd21j18; source [find target/at91samdXX.cfg]; adapter_khz 250; adapter_nsrst_delay 100; adapter_nsrst_assert_width 100" -c "init; targets; reset halt; program /home/pi/Dexter/BrickPi3/Firmware/brickpi3_firmware.bin verify; reset" -c "shutdown"
