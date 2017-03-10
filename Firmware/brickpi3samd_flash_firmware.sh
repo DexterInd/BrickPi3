ad0b361eb87359a67691d4efac196bc52a985752
Shoban94 <shobannarayan@gmail.com>
2017-03-10 14:31:57 +0530

    Update REPO_PATH

--- Firmware/brickpi3samd_flash_firmware.sh
+++ Firmware/brickpi3samd_flash_firmware.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-REPO_PATH=$(sudo find / -name "BrickPi3" | head -1)
+REPO_PATH=$(sudo find / -path "*BrickPi3/Software/Python" | grep -E -o "^(.*?\\BrickPi3)"| head -1)
 echo "Updating the BrickPi3 SAMD Firmware with '$REPO_PATH/Firmware/brickpi3_firmware.bin'."
 sudo openocd -f interface/raspberrypi2-native.cfg -c "transport select swd; set CHIPNAME at91samd21j18; source [find target/at91samdXX.cfg]; adapter_khz 100; adapter_nsrst_delay 100; adapter_nsrst_assert_width 100" -c "init; targets; reset halt; program $REPO_PATH/Firmware/brickpi3_firmware.bin verify; reset" -c "shutdown"
