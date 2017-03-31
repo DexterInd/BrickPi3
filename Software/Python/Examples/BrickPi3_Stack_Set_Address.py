60260794c3648a945a699ae7759d30e109663a99
Matthew Richardson <matt@dexterindustries.com>
2017-03-31 11:10:40 -0400

    Update Python stack example for three BrickPi3s

--- Software/Python/Examples/BrickPi3_Stack_Set_Address.py
+++ Software/Python/Examples/BrickPi3_Stack_Set_Address.py
@@ -15,12 +15,15 @@ from __future__ import division       #                           ''
 import time     # import the time library for the sleep function
 import brickpi3 # import the BrickPi3 drivers
 
-brickpi3.set_address(2, "") # set all attached BrickPi3s to address 2
+#brickpi3.set_address(2, "") # set all attached BrickPi3s to address 2
+brickpi3.set_address(2, "7FCDC951514D4D5439202020FF0E0911")
 brickpi3.set_address(3, "192A0F96514D4D5438202020FF080C23") # set BrickPi3 with serial number ID "192A0F96514D4D5438202020FF080C23" to address 3
+brickpi3.set_address(4, "84FBCC9D514D4D5439202020FF0E070F")
 
 try:
     BP3_2 = brickpi3.BrickPi3(2) # Create an instance of the BrickPi3 class. BP3_2 will be the BrickPi3 object for the BrickPi3 with address 2.
     BP3_3 = brickpi3.BrickPi3(3) # Create an instance of the BrickPi3 class. BP3_3 will be the BrickPi3 object for the BrickPi3 with address 3.
+    BP3_4 = brickpi3.BrickPi3(4)
     
     # Each of the following .get functions return a value that we want to display.
     print("BrickPi3 Address: 2")
@@ -46,6 +49,19 @@ try:
     print("9v voltage      : ", BP3_3.get_voltage_9v()      ) # read and display the current 9v regulator voltage
     print("5v voltage      : ", BP3_3.get_voltage_5v()      ) # read and display the current 5v regulator voltage
     print("3.3v voltage    : ", BP3_3.get_voltage_3v3()     ) # read and display the current 3.3v regulator voltage
+    
+    print("")
+    
+    print("BrickPi3 Address: 4")
+    print("Manufacturer    : ", BP3_4.get_manufacturer()    ) # read and display the serial number
+    print("Board           : ", BP3_4.get_board()           ) # read and display the serial number
+    print("Serial Number   : ", BP3_4.get_id()              ) # read and display the serial number
+    print("Hardware version: ", BP3_4.get_version_hardware()) # read and display the hardware version
+    print("Firmware version: ", BP3_4.get_version_firmware()) # read and display the firmware version
+    print("Battery voltage : ", BP3_4.get_voltage_battery() ) # read and display the current battery voltage
+    print("9v voltage      : ", BP3_4.get_voltage_9v()      ) # read and display the current 9v regulator voltage
+    print("5v voltage      : ", BP3_4.get_voltage_5v()      ) # read and display the current 5v regulator voltage
+    print("3.3v voltage    : ", BP3_4.get_voltage_3v3()     ) # read and display the current 3.3v regulator voltage
     
 except IOError as error:
     print(error)
