a653f89a5187ee23c7f0a1e6fb0e7a5156ea5efc
Matthew Richardson <matt@dexterindustries.com>
2017-03-31 11:09:45 -0400

    Add NXT Light sensor Python example

---
+++ Software/Python/Examples/NXT-Light_Sensor.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+#
+# https://www.dexterindustries.com/BrickPi/
+# https://github.com/DexterInd/BrickPi3
+#
+# Copyright (c) 2016 Dexter Industries
+# Released under the MIT license (http://choosealicense.com/licenses/mit/).
+# For more information, see https://github.com/DexterInd/BrickPi3/blob/master/LICENSE.md
+#
+# This code is an example for reading an NXT light sensor connected to PORT_1 of the BrickPi3
+# 
+# Hardware: Connect an NXT light sensor to BrickPi3 Port 1.
+# 
+# Results:  When you run this program, you should see the value from the light sensor.
+
+from __future__ import print_function # use python 3 syntax but make it compatible with python 2
+from __future__ import division       #                           ''
+
+import time     # import the time library for the sleep function
+import brickpi3 # import the BrickPi3 drivers
+
+BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be the BrickPi3 object.
+
+# Configure for an NXT light sensor.
+# BP.set_sensor_type configures the BrickPi3 for a specific sensor.
+# BP.PORT_1 specifies that the sensor will be on sensor port 1.
+# BP.SENSOR_TYPE.NXT_LIGHT_ON specifies that the sensor will be an NXT light sensor.
+BP.set_sensor_type(BP.PORT_1, BP.SENSOR_TYPE.NXT_LIGHT_ON)
+
+try:
+    while True:
+        # read and display the sensor value
+        # BP.get_sensor retrieves a sensor value.
+        # BP.PORT_1 specifies that we are looking for the value of sensor port 1.
+        # BP.get_sensor returns the sensor value (what we want to display).
+        try:
+            value = BP.get_sensor(BP.PORT_1)
+            print(value)                         # print the value
+        except brickpi3.SensorError as error:
+            print(error)
+        
+        time.sleep(0.02)  # delay for 0.02 seconds (20ms) to reduce the Raspberry Pi CPU load.
+
+except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
+    BP.reset_all()        # Unconfigure the sensors, disable the motors, and restore the LED to the control of the BrickPi3 firmware.
