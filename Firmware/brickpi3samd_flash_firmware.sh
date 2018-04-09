fbd299a3e04f74b855182f0690a2f3e6c996fe11
Matthew Richardson <matt@dexterindustries.com>
2018-04-09 13:47:40 -0400

    Update firmware flash script for RPi 3B+
    Update the firmware flash script to support the Raspberry Pi 3B+.

--- Firmware/brickpi3samd_flash_firmware.sh
+++ Firmware/brickpi3samd_flash_firmware.sh
@@ -8,17 +8,17 @@ FILE="$REPO_PATH/Firmware/brickpi3_firmware.bin"
 # make sure the firmware file exists
 if [ -f "$FILE" ]; then
     echo "Updating the BrickPi3 SAMD Firmware with '$FILE'"
-    
+
     # check the RPi version
     RPI_VERSION=$(python -c "import auto_detect_rpi; print auto_detect_rpi.getRPIGenerationCode()")
     # set default to RPi 1 interface config file
     INTERFACE_FILE="rpi1.cfg"
-    if [ "$RPI_VERSION" == "RPI2" ] || [ "$RPI_VERSION" == "RPI3" ]; then
+    if [ "$RPI_VERSION" == "RPI2" ] || [ "$RPI_VERSION" == "RPI3" ] || [ "$RPI_VERSION" == "RPI3B+" ]; then
         # use RPi 2 interface config file
         INTERFACE_FILE="rpi2.cfg"
     fi
     echo "Using interface file '$INTERFACE_FILE'"
-    
+
     # flash the firmware
     sudo openocd -f interface/$INTERFACE_FILE -c "transport select swd; set CHIPNAME at91samd21j18; source [find target/at91samdXX.cfg]; adapter_khz 50; adapter_nsrst_delay 100; adapter_nsrst_assert_width 100" -c "init; targets; reset halt; program $FILE verify; reset" -c "shutdown"
 
