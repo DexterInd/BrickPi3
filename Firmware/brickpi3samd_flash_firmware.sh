017ab052c0ebfd7d1e3fbb0326df0d58cafc7949
Matthew Richardson <matt@dexterindustries.com>
2019-07-08 15:06:46 -0400

    Add support for RPI4

--- Firmware/brickpi3samd_flash_firmware.sh
+++ Firmware/brickpi3samd_flash_firmware.sh
@@ -1,27 +1,35 @@
 #! /bin/bash
-# determine repo path (install directory)
-REPO_PATH=$(readlink -f $(dirname $0) | grep -E -o "^(.*?\\BrickPi3)")
 
-# specify full file name, including repo path
-FILE="$REPO_PATH/Firmware/brickpi3_firmware.bin"
+# check the RPi version
+RPI_VERSION=$(python3 -c "import auto_detect_rpi; print (auto_detect_rpi.getRPIGenerationCode())")
 
-# make sure the firmware file exists
-if [ -f "$FILE" ]; then
-    echo "Updating the BrickPi3 SAMD Firmware with '$FILE'"
-
-    # check the RPi version
-    RPI_VERSION=$(python -c "import auto_detect_rpi; print auto_detect_rpi.getRPIGenerationCode()")
-    # set default to RPi 1 interface config file
+INTERFACE_FILE="none"
+if [ "$RPI_VERSION" == "RPI1" ] || [ "$RPI_VERSION" == "RPI0" ]; then
+    # use rpi1 interface config file
     INTERFACE_FILE="rpi1.cfg"
-    if [ "$RPI_VERSION" == "RPI2" ] || [ "$RPI_VERSION" == "RPI3" ] || [ "$RPI_VERSION" == "RPI3B+" ]; then
-        # use RPi 2 interface config file
-        INTERFACE_FILE="rpi2.cfg"
-    fi
-    echo "Using interface file '$INTERFACE_FILE'"
+elif [ "$RPI_VERSION" == "RPI2" ] || [ "$RPI_VERSION" == "RPI3" ] || [ "$RPI_VERSION" == "RPI3B+" ] || [ "$RPI_VERSION" == "RPI3A+" ] || [ "$RPI_VERSION" == "RPI4" ]; then
+    # use rpi2 interface config file
+    INTERFACE_FILE="rpi2.cfg"
+fi
 
-    # flash the firmware
-    sudo openocd -f interface/$INTERFACE_FILE -c "transport select swd; set CHIPNAME at91samd21j18; source [find target/at91samdXX.cfg]; adapter_khz 50; adapter_nsrst_delay 100; adapter_nsrst_assert_width 100" -c "init; targets; reset halt; program $FILE verify; reset" -c "shutdown"
-
+if [ "$INTERFACE_FILE" == "none" ]; then
+    # unsupported RPI
+    echo "Unsupported RPi version '$RPI_VERSION'. Please report to support@dexterindustries.com"
 else
-    echo "Firmware file '$FILE' does not exist."
+    echo "Using interface file '$INTERFACE_FILE' for RPi version '$RPI_VERSION'."
+
+    # Read the path of the BrickPi3 repository
+    REPO_PATH=$(readlink -f $(dirname $0) | grep -E -o "^(.*?\\BrickPi3)")
+
+    # Get the absolute path of the latest Firmware update
+    FIRMWARE_FILE=$(sudo find "$REPO_PATH"/Firmware/ -maxdepth 1 -name *.bin)
+
+    if [ "$FIRMWARE_FILE" == "" ]; then
+        echo "Failed to find firmware file."
+    else
+        echo "Updating the BrickPi3 Firmware with '$FIRMWARE_FILE'"
+
+        # flash the firmware
+        sudo openocd -f interface/$INTERFACE_FILE -c "transport select swd; set CHIPNAME at91samd21j18; source [find target/at91samdXX.cfg]; adapter_khz 50; adapter_nsrst_delay 100; adapter_nsrst_assert_width 100" -c "init; targets; reset halt; program $FIRMWARE_FILE verify; reset" -c "shutdown"
+    fi
 fi
